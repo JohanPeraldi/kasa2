9863f50de1a4bc6448e5e3bbf753f943d7290f34
Johan Peraldi <johan.peraldi@gmail.com>
2022-12-01 18:03:25 +0200

    Add useState hook to Collapse component

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,9 +1,12 @@
+import React, { useState } from 'react';
 import styles from './Collapse.module.css';
 
 export default function Collapse(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <article>
-      <div className={styles.wrapper}>
+      <div className={styles.wrapper} onClick={() => setIsOpen(!isOpen)}>
         <h2 className={styles.title}>{props.title}</h2>
         <svg
           width="24"
@@ -11,6 +14,7 @@ export default function Collapse(props) {
           viewBox="0 0 24 15"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
+          className={!isOpen && styles.down}
         >
           <path
             d="M21.7269 14.1403L23.8599 11.9951L11.93 0.0764155L7.19578e-05 12.0072L2.13299 14.1403L11.93 4.34257L21.7269 14.1403Z"
@@ -18,7 +22,11 @@ export default function Collapse(props) {
           />
         </svg>
       </div>
-      <p className={styles.description}>{props.description}</p>
+      {isOpen && (
+        <p className={styles.description} style={props.visible}>
+          {props.description}
+        </p>
+      )}
     </article>
   );
 }
