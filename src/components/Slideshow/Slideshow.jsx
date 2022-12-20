c5266defa38e4efbecaa57493b418b895242a280
Johan Peraldi <johan.peraldi@gmail.com>
2022-12-20 16:42:02 +0100

    Add chevrons to Slideshow components & adjust styles

--- src/components/Slideshow/Slideshow.jsx
+++ src/components/Slideshow/Slideshow.jsx
@@ -1,11 +1,25 @@
+import chevronLeft from '../../assets/svg/chevron-left.svg';
+import chevronRight from '../../assets/svg/chevron-right.svg';
 import styles from './Slideshow.module.css';
 
 export default function Slideshow(props) {
   const pictures = props.pictures;
 
   return (
-    <div className={styles.wrapper}>
-      <img src={pictures[0]} alt="" />
-    </div>
+    pictures.length > 0 && (
+      <div className={styles.wrapper}>
+        <img
+          src={chevronLeft}
+          className={`${styles.chevron} ${styles['chevron-left']}`}
+          alt=""
+        />
+        <img src={pictures[0]} className={styles.picture} alt="" />
+        <img
+          src={chevronRight}
+          className={`${styles.chevron} ${styles['chevron-right']}`}
+          alt=""
+        />
+      </div>
+    )
   );
 }
