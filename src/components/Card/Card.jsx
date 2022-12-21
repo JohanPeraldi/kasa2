9c29b4fbfb26db4943715d6388cf35f3c08dc151
Johan Peraldi <johan.peraldi@gmail.com>
2022-12-21 21:55:17 +0100

    Add linear-gradient over apartment images on homepage

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,10 @@ import styles from './Card.module.css';
 
 export default function Card(props) {
   const articleStyle = {
-    background: 'url(' + props.cover + ')',
+    background:
+      'linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 100%), url(' +
+      props.cover +
+      ')',
   };
 
   return (
